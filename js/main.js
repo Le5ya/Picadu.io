ef67d121428db012122b7f50d4f433cbb9463b83
Leya Leya <liliatolstyh@yandex.ru>
2020-11-11 22:21:51 +0300

    first day

---
+++ js/main.js
@@ -0,0 +1,102 @@
+// Создаем переменную, в которую положим кнопку меню
+let menuToggle = document.querySelector('#menu-toggle');
+// Создаем переменную, в которую положим меню
+let menu = document.querySelector('.sidebar');
+// отслеживаем клик по кнопке меню и запускаем функцию 
+menuToggle.addEventListener('click', function (event) {
+  // отменяем стандартное поведение ссылки
+  event.preventDefault();
+  // вешаем класс на меню, когда кликнули по кнопке меню 
+  menu.classList.toggle('visible');
+})
+
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-password');
+const loginSignup = document.querySelector('.login-signup');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
+
+
+const listUsers = [
+  {
+    email:'maks@mail.com',
+    password: '12345',
+    displayName: 'MaksJS'
+  },
+  {
+    email:'kate@mail.com',
+    password: '123456',
+    displayName: 'KateJS'
+  },
+];
+
+const setUsers = {
+  user: null,
+  logIn(email, password) {
+    const user = this.getUser(email);
+    if(user && user.password === password) {
+      this.authorizedUser(user)
+      handler();
+    } else {
+      alert ('Почта или пароль указаны неверно')
+    }
+  },
+  logOut() {
+    console.log('logOut');
+  },
+  signUp(email, password, handler) {
+    if(!this.getUser(email)) {
+      const user = {email, password, displayName: email};
+      listUsers.push(user)
+      this.authorizedUser(user)
+      handler();
+    } else {
+      alert('Пользователь с таким email уже зарегистрирован')
+    }
+  }, 
+  getUser(email) {
+    return listUsers.find(item => item.email === email)
+  },
+  authorizedUser(user) {
+    this.user = user;
+  }
+};
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+  console.log('user:', user);
+
+  if(user) {
+    console.log("It's Him");
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    console.log("Nobody");
+    loginElem.style.display = '';
+    userElem.style.display = 'none'; 
+  }
+};
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+});
+loginSignup.addEventListener('click', event => {
+  event.preventDefault();
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+
+});
+
+toggleAuthDom();
+
