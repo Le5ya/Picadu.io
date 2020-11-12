c1d1cf941a16486bfc88247ef6ea9751ebda218d
Leya Leya <liliatolstyh@yandex.ru>
2020-11-12 16:01:34 +0300

    create: redaction

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ menuToggle.addEventListener('click', function (event) {
   menu.classList.toggle('visible');
 })
 
+const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
+
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
@@ -18,6 +20,14 @@ const loginSignup = document.querySelector('.login-signup');
 
 const userElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editContainer = document.querySelector('.edit-container');
+
+const editUsername = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
+const userAvatarElem = document.querySelector('.user-avatar');
+
 
 
 
@@ -36,7 +46,11 @@ const listUsers = [
 
 const setUsers = {
   user: null,
-  logIn(email, password) {
+  logIn(email, password, handler) {
+    if(!regExpValidEmail.test(email)) {
+       alert ('email is invalid');
+       return;
+  }
     const user = this.getUser(email);
     if(user && user.password === password) {
       this.authorizedUser(user)
@@ -45,11 +59,21 @@ const setUsers = {
       alert ('Почта или пароль указаны неверно')
     }
   },
-  logOut() {
-    console.log('logOut');
+  logOut(handler) {
+    this.user = null;
+    handler();
   },
   signUp(email, password, handler) {
-    if(!this.getUser(email)) {
+      if(!regExpValidEmail.test(email)) {
+      alert ('email is invalid');
+      return;
+      }
+    if (!email.trim() || !password.trim()) {
+      alert('Введите данные')
+      return;
+    }
+
+    if(!this.getUser(email))  {
       const user = {email, password, displayName: email};
       listUsers.push(user)
       this.authorizedUser(user)
@@ -57,7 +81,16 @@ const setUsers = {
     } else {
       alert('Пользователь с таким email уже зарегистрирован')
     }
-  }, 
+  },
+  editUser(userName, userPhoto, handler ) {
+      if (userName) {
+        this.user.displayName = userName;
+      }
+      if (userPhoto) {
+        this.user.photo = userPhoto;
+      }
+      handler();
+  },
   getUser(email) {
     return listUsers.find(item => item.email === email)
   },
@@ -71,12 +104,11 @@ const toggleAuthDom = () => {
   console.log('user:', user);
 
   if(user) {
-    console.log("It's Him");
     loginElem.style.display = 'none';
     userElem.style.display = '';
     userNameElem.textContent = user.displayName;
+    userAvatarElem.src = user.photo ? user.photo : userAvatarElem.src;
   } else {
-    console.log("Nobody");
     loginElem.style.display = '';
     userElem.style.display = 'none'; 
   }
@@ -88,6 +120,7 @@ loginForm.addEventListener('submit', event => {
   const passwordValue = passwordInput.value;
 
   setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+  loginForm.reset();
 });
 loginSignup.addEventListener('click', event => {
   event.preventDefault();
@@ -95,8 +128,24 @@ loginSignup.addEventListener('click', event => {
   const passwordValue = passwordInput.value;
 
   setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+  loginForm.reset();
+
+  exitElem.addEventListener('click', event => {
+    event.preventDefault();
+    setUsers.logOut(toggleAuthDom);
+  })
 
 });
+
+editElem.addEventListener('click', event => {
+  event.preventDefault();
+  editContainer.classList.toggle('visible');
+});
+editContainer.addEventListener('click', event => {
+  event.preventDefault();
+
+  setUsers.editUser(editUsername.value, editPhotoURL.value, toggleAuthDom)
+})
 
 toggleAuthDom();
 
